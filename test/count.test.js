0be9792caef73c512491e9ea14ec7d37660d2a41
ParkYoungWoong <thesecon@gmail.com>
2024-08-14 02:21:13 +0900

    테스트가 성공하도록 수정.

--- test/count.test.js
+++ test/count.test.js
@@ -4,7 +4,7 @@ import { count, increase } from '../count.js'
 it('increase 함수를 호출하면, count 데이터가 1 증가!', () => {
   assert.equal(count, 0)
   increase()
-  assert.equal(count, 7) // 실패!
+  assert.equal(count, 1) // 성공!
 })
 
 it('increase 함수를 호출하면, count 값을 반환!', () => {
